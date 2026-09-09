9ddad742b5bdc0117992388ea62e1d7de2fa83c2
vansh sharma <vs6905397@gmail.com>
2026-09-09 21:09:41 +0530

    cors fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://lms-learning-management-system-k7zc.onrender.com",
+    baseURL: "https://lms-learning-management-system-k7zc.onrender.com/api",
     withCredentials: true,
 });
 
